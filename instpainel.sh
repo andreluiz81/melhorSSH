fdc4449909b1911e105c427d20b8d24e86e6aa84
HB2081 <luizandre1981@gmail.com>
2022-05-23 14:11:46 -0300

    Update instpainel.sh

--- instpainel.sh
+++ instpainel.sh
@@ -6,7 +6,7 @@ dpkg-reconfigure --frontend noninteractive tzdata > /dev/null 2>&1
 clear
 echo -e "\E[44;1;37m    INSTALAR PAINEL PWEB GESTOR-SSH     \E[0m" 
 echo ""
-echo -e "                 \033[1;31mBy @nandoslayer\033[1;36m"
+echo -e "                 \033[1;31mBy @andreejesus\033[1;36m"
 echo ""
 echo -ne "\n\033[1;32mDE UM ENTER PARA \033[1;33mCONTINUAR...\033[1;37m: "; read -r
 clear
@@ -18,9 +18,9 @@ clear
 [[ ! -d /etc/SSHPlus/Painel ]] && mkdir /etc/SSHPlus/Painel
 rm *.sh* > /dev/null 2>&1
 rm *.zip > /dev/null 2>&1
-wget github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/painel.zip.001 > /dev/null 2>&1
-wget github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/painel.zip.002 > /dev/null 2>&1
-wget github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/painel.zip.003 > /dev/null 2>&1
+wget github.com/andreluiz81/melhorSSH/raw/ntech/gestorssh/painel.zip.001 > /dev/null 2>&1
+wget github.com/andreluiz81/melhorSSH/raw/ntech/gestorssh/painel.zip.002 > /dev/null 2>&1
+wget github.com/andreluiz81/melhorSSH/raw/ntech/gestorssh/painel.zip.003 > /dev/null 2>&1
 cat painel.zip* > painelweb.zip && zip -F painelweb.zip > /dev/null 2>&1
 rm -rf /opt/painel > /dev/null 2>&1
 unzip painelweb.zip -d  /opt/painel/ > /dev/null 2>&1
@@ -33,27 +33,27 @@ sleep 1
 rm /bin/pweb > /dev/null 2>&1
 sleep 5
 cd /bin || exit
-wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/pweb > /dev/null 2>&1
+wget https://github.com/andreluiz81/melhorSSH/raw/ntech/gestorssh/pweb > /dev/null 2>&1
 chmod 777 pweb > /dev/null 2>&1
 clear
 [[ ! -d /bin/ppweb ]] && mkdir /bin/ppweb
 cd /bin/ppweb || exit
 rm *.sh versao* > /dev/null 2>&1
-wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/install.sh > /dev/null 2>&1
-wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/ubuinst.sh > /dev/null 2>&1
-wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/senharoot.sh > /dev/null 2>&1
-wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/restbanco.sh > /dev/null 2>&1
-wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/restbanco18.sh > /dev/null 2>&1
-wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/empresa.sh > /dev/null 2>&1
-wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/updatepainel.sh > /dev/null 2>&1
-wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/updatepainel18.sh > /dev/null 2>&1
-wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/updatepainelarm.sh > /dev/null 2>&1
-wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/updatepainelarm18.sh > /dev/null 2>&1
-wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/versao > /dev/null 2>&1
+wget https://github.com/andreluiz81/melhorSSH/raw/ntech/gestorssh/install.sh > /dev/null 2>&1
+wget https://github.com/andreluiz81/melhorSSH/raw/ntech/gestorssh/ubuinst.sh > /dev/null 2>&1
+wget https://github.com/andreluiz81/melhorSSH/raw/ntech/gestorssh/senharoot.sh > /dev/null 2>&1
+wget https://github.com/andreluiz81/melhorSSH/raw/ntech/gestorssh/restbanco.sh > /dev/null 2>&1
+wget https://github.com/andreluiz81/melhorSSH/raw/ntech/gestorssh/restbanco18.sh > /dev/null 2>&1
+wget https://github.com/andreluiz81/melhorSSH/raw/ntech/gestorssh/empresa.sh > /dev/null 2>&1
+wget https://github.com/andreluiz81/melhorSSH/raw/ntech/gestorssh/updatepainel.sh > /dev/null 2>&1
+wget https://github.com/andreluiz81/melhorSSH/raw/ntech/gestorssh/updatepainel18.sh > /dev/null 2>&1
+wget https://github.com/andreluiz81/melhorSSH/raw/ntech/gestorssh/updatepainelarm.sh > /dev/null 2>&1
+wget https://github.com/andreluiz81/melhorSSH/raw/ntech/gestorssh/updatepainelarm18.sh > /dev/null 2>&1
+wget https://github.com/andreluiz81/melhorSSH/raw/ntech/gestorssh/versao > /dev/null 2>&1
 chmod 777 *.sh > /dev/null 2>&1
 echo -e "        \033[1;33m • \033[1;32mINSTALAÇÃO CONCLUÍDA\033[1;33m • \033[0m"
 echo ""
 echo -e "\033[1;31m \033[1;33mCOMANDO PRINCIPAL: \033[1;32mpweb\033[0m"
-echo -e "\033[1;33m MAIS INFORMAÇÕES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@nandoslayer\033[0m"
+echo -e "\033[1;33m MAIS INFORMAÇÕES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@andreejesus\033[0m"
 cat /dev/null > ~/.bash_history && history -c
 exit
